e394b40c565cdcf058c0c993ac50ec6b135feb32
Shilo <shilo86@gmail.com>
2024-03-06 17:52:33 -0800

    feat: improved web demo

--- godot/src/icon_url_button.gd
+++ godot/src/icon_url_button.gd
@@ -1,5 +1,6 @@
-extends Control
+extends TextureButton
 
+@export var url: String = "https://godotengine.org"
 @export var spin_duration: float = -20
 
 func _ready():
@@ -7,3 +8,6 @@ func _ready():
 
 func _process(delta):
 	rotation += TAU * delta / spin_duration
+
+func _pressed():
+	OS.shell_open(url)
