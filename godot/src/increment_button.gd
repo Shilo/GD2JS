f7f7c072a8059af92f0a507ad4aaa81d28c79bf8
Shilo <shilo86@gmail.com>
2024-03-09 15:56:43 -0800

    chore: test virtual keyboard

--- godot/src/increment_button.gd
+++ godot/src/increment_button.gd
@@ -3,4 +3,8 @@ extends Button
 @export var count: CountLabel
 
 func _pressed():
-	count.value += 1
+	count.value += 2
+	if DisplayServer.virtual_keyboard_get_height() > 0:
+		DisplayServer.virtual_keyboard_hide()
+	else:
+		DisplayServer.virtual_keyboard_show("")
