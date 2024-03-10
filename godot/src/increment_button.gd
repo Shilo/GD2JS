527f1cf487714cfcc1f96aa9fea8d48aff78a25a
Shilo <shilo86@gmail.com>
2024-03-09 16:11:01 -0800

    Revert "chore: test virtual keyboard"
    This reverts commit f7f7c072a8059af92f0a507ad4aaa81d28c79bf8.

--- godot/src/increment_button.gd
+++ godot/src/increment_button.gd
@@ -3,8 +3,4 @@ extends Button
 @export var count: CountLabel
 
 func _pressed():
-	count.value += 2
-	if DisplayServer.virtual_keyboard_get_height() > 0:
-		DisplayServer.virtual_keyboard_hide()
-	else:
-		DisplayServer.virtual_keyboard_show("")
+	count.value += 1
