3bb3a016abb3b9893e839b716030ce74d5ab7a2a
Shilo <shilo86@gmail.com>
2024-03-09 16:10:55 -0800

    Revert "test"
    This reverts commit a578f4cfa18ad6e5b4a9ea9e698ceaa41f52d81d.

--- godot/src/increment_button.gd
+++ godot/src/increment_button.gd
@@ -3,5 +3,8 @@ extends Button
 @export var count: CountLabel
 
 func _pressed():
-	count.value += 3
-	DisplayServer.virtual_keyboard_show("")
+	count.value += 2
+	if DisplayServer.virtual_keyboard_get_height() > 0:
+		DisplayServer.virtual_keyboard_hide()
+	else:
+		DisplayServer.virtual_keyboard_show("")
