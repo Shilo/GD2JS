2d5955254dfa38adb939b602b74766eb47b2995b
Shilo <shilo86@gmail.com>
2024-04-19 02:43:07 -0700

    feat: javascript api

--- godot/addons/xida/gd2js/gd2js.gd
+++ godot/addons/xida/gd2js/gd2js.gd
@@ -1,16 +1,25 @@
 class_name GD2JS extends Object
 
-static var ENABLED: bool = OS.has_feature("web")
+static var enabled: bool = OS.has_feature("web"):
+	set(__): pass
+
+static var window: JavaScriptObject = JavaScriptBridge.get_interface("parent"):
+	set(__): pass
+
+static var gd2js: JavaScriptObject:
+	get: return window.GD2JS if window else null
+	set(__): pass
 
 static func _static_init() -> void:
 	if !_is_enabled(): return
 	
-	print("is web")
+	var js_script: GDScript = load("res://addons/xida/gd2js/gd2js.js.gd")
+	JavaScriptBridge.eval(js_script.js)
 
 static func add_event_listener() -> void:
 	pass
 
 static func _is_enabled() -> bool:
-	if (!ENABLED):
+	if (!enabled):
 		push_warning("[GDJS] Feature not allowed in non-web build.")
-	return ENABLED
+	return enabled
