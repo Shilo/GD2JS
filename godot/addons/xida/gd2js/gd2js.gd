4a1ea1c5fe09e42dd4da6c6c1bd06209500e7c18
Shilo <shilo86@gmail.com>
2024-04-21 04:55:11 -0700

    feat: godot api

--- godot/addons/xida/gd2js/gd2js.gd
+++ godot/addons/xida/gd2js/gd2js.gd
@@ -6,7 +6,7 @@ var enabled: bool = OS.has_feature("web"):
 var window: JavaScriptObject = JavaScriptBridge.get_interface("parent"):
 	set(__): pass
 
-var gd2js: JavaScriptObject:
+var js: JavaScriptObject:
 	get: return window.GD2JS if window else null
 
 var _js_script_path: String:
@@ -15,6 +15,7 @@ var _js_script_path: String:
 		return path.insert(path.length() - 3, ".js")
 
 var _gd_eval_callback: JavaScriptObject
+var _callbacks: Dictionary
 
 func _init() -> void:
 	if !enabled: return
@@ -36,15 +37,89 @@ func set_gd_eval(enable: bool) -> void:
 	if !_is_enabled(): return
 	
 	_gd_eval_callback = JavaScriptBridge.create_callback(_gd_eval) if enable else null
-	gd2js._eval = _gd_eval_callback
+	js._eval = _gd_eval_callback
 
 func _gd_eval(code: Array) -> Error:
 	for code_block in code:
 		var script := GDScript.new()
-		script.source_code = "static func _static_init():\n\t" + code_block.strip_edges().replace("\n", ";")
+		script.source_code = "static func _static_init():" + code_block.strip_edges().replace("\n", ";")
 		
 		var error := script.reload()
 		if error:
 			return error
 	
 	return OK
+
+func js_connect(type: String, callable: Callable, options: Variant = false) -> JavaScriptObject:
+	if !_is_enabled(): return
+	
+	var wrapped_callback := JavaScriptBridge.create_callback(func(args): callable.callv(args))
+	var result: JavaScriptObject = js.addEventListener(type, wrapped_callback, options)
+	
+	if !_callbacks[type]:
+		_callbacks[type] = []
+	_callbacks[type].push_back({
+		"callback": callable,
+		"wrapped_callback": wrapped_callback
+	})
+	
+	return result
+
+func js_is_connected(type: String, callable: Callable) -> bool:
+	if !_is_enabled(): return false
+	
+	return js.isEventListener(type, callable)
+
+func js_disconnect(type: String, callable: Callable, options: Variant = false) -> bool:
+	if !_is_enabled(): return false
+	
+	var callbacks: Array = _callbacks[type]
+	if !callbacks: return false
+	
+	var index: int = -1
+	var i: int = 0
+	for c in callbacks:
+		if c.callback == callable:
+			index = i
+			break
+		i += 1
+	if index == -1: return false
+	
+	var result = js.removeEventListener(type, callbacks[index].wrapped_callback, options)
+	
+	callbacks.remove_at(index)
+	if callbacks.size() == 0:
+		_callbacks.erase(type)
+	
+	return result
+
+func js_disconnect_all(type: String = "") -> bool:
+	if !_is_enabled(): return false
+	
+	if !type:
+		for callback_type in _callbacks:
+			js.removeAllEventListeners(callback_type)
+		return true
+	
+	var result: bool = js.removeAllEventListeners(type)
+	
+	var callbacks = _callbacks[type]
+	if callbacks:
+		_callbacks.erase(type)
+	
+	return result
+
+func js_emit_signal(type:String, arg1: Variant = NAN, arg2: Variant = NAN, arg3: Variant = NAN, arg4: Variant = NAN) -> JavaScriptObject:
+	if !_is_enabled(): return
+	
+	if is_nan(arg1): return js.dispatchEvent(type)
+	if is_nan(arg2): return js.dispatchEvent(type, arg1)
+	if is_nan(arg3): return js.dispatchEvent(type, arg1, arg2)
+	if is_nan(arg4): return js.dispatchEvent(type, arg1, arg2, arg3)
+	return js.dispatchEvent(type, arg1, arg2, arg3, arg4)
+
+func js_emit_signal_v(type:String, args: Array[Variant]) -> JavaScriptObject:
+	if !_is_enabled(): return
+	
+	args.push_front(type)
+	return js_emit_signal.callv(args)
